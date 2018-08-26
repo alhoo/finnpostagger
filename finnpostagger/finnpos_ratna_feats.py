ae2ffaba7463a6e6d2ec4d2bb252380622f3822d
Lasse Hyyrynen <lasse.hyyrynen@utopiaanalytics.com>
2018-08-26 07:44:19 +0300

    remove redundant code from the original implementation

--- finnpostagger/finnpos_ratna_feats.py
+++ finnpostagger/finnpos_ratna_feats.py
@@ -4,6 +4,7 @@
 
  file    finnpos-ratna-feats.py                                                     
  Author  Miikka Silfverberg                                                 
+ Modifications  Lasse Hyyrynen
  brief   Extract Ratnaparkhi 1996 features from an input-file.              
 
          (Adwait Ratnaparkhi: A Maximum Entropy Model for Part-of-Speech    
@@ -27,7 +28,6 @@
   limitations under the License.
 """
 
-from sys import stdin, stdout, stderr, argv
 from re  import match
 from os  import linesep
 
@@ -46,8 +46,6 @@ def convert(ingen, freq_words):
         This function doesn't check its argument streams. """
 
     i = 0
-
-    ofile = stdout
 
     while True:
         sentence = []
@@ -128,96 +126,6 @@ def convert(ingen, freq_words):
         
     return EXIT_SUCCESS
 
-def main(freq_words):
-    """ Read input from ifile and write extracted features to ofile. 
-        This function doesn't check its argument streams. """
-
-    i = 0
-
-    ofile = stdout
-
-    while True:
-        sentence = []
-        for word in stdin:
-            word = word.rstrip()
-            if word == '':
-                break
-            sentence.append([word])
-
-        if not sentence:
-            break
-
-        # Extract features for each sentence and write output to ofile.
-        for i, line in enumerate(sentence):
-            wf    = '_'
-            feats = '_'
-            lemma = '_'
-            label = '_'
-            ann   = '_'
-            
-            if len(line) == 1:
-                wf = line[0]
-                lemma, label, ann = '_', '_', '_'
-            elif len(line) == 3:
-                wf, lemma, label = line
-                ann = "_"
-            else:
-                wf, feats, lemma, label, ann = line
-
-            features = []                
-
-            if feats != '_':
-                features = feats.split(' ')
-        
-            if not ann in ['_', '']:
-                lemma_list = ann
-                if ' ' in ann:
-                    lemma_list = ann[:ann.find(' ')]
-                
-                label_feats = [ "FEAT:" + label for label in 
-                                map(lambda x: x[0], eval(lemma_list)) ]
-
-                if len(label_feats) != 0:
-                    features += label_feats
-                else:
-                    features.append("NO_LABELS")
-
-            features.append('PPWORD=' + get_wf(i - 2, sentence))
-            features.append('PWORD='  + get_wf(i - 1, sentence))
-            features.append('WORD='   + wf)
-            features.append('WORD_LEN='   + str(len(wf)))
-            features.append('NWORD='  + get_wf(i + 1, sentence))
-            features.append('NNWORD=' + get_wf(i + 2, sentence))
-            
-            features.append('PWORDPAIR='  + get_wf(i - 1, sentence) + "_" + wf)
-            features.append('NWORDPAIR='  + wf + "_" + get_wf(i + 1, sentence))
-            
-            features.append("LC_WORD=" + wf.lower())
-            
-            if not wf in freq_words:
-                features += get_suffixes(wf)
-                features += get_prefixes(wf)
-            
-                features.append(has_uc(wf))
-                features.append(has_digit(wf))
-                features.append(has_dash(wf))
-            
-            feat_str = " ".join(filter(None, features))
-        
-        
-            ofile.write(("%s\t%s\t%s\t%s\t%s" + linesep) 
-                        % 
-                        (wf, feat_str, lemma, label, ann))
-        
-        ofile.write(linesep)
-        
-        # Ensure that the line is written immediately so that features
-        # get passed to finnpos-label in real time when labeling text
-        # from a stream.
-        ofile.flush()
-
-    return EXIT_SUCCESS
-
 def get_wf(i, sentence):
     return BOUNDARY if i < 0 or i + 1 > len(sentence) else sentence[i][0]
 
@@ -237,12 +145,3 @@ def has_digit(wf):
 
 def has_dash(wf):
     return "HAS_DASH" if match(".*-.*", wf) else None
-
-if __name__ == "__main__":
-    if len(argv) != 2:
-        stderr.write("cat data | %s freq_word_list\n" % argv[0])
-        exit(1)
-
-    freq_words = set(open(argv[1]).read().split('\n'))
-
-    exit(main(freq_words))
